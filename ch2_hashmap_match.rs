be14673b4b53331fdd7fa6f9b43f7308506d0978
Junichi MARUYAMA <junichi.maruyama.rg@riken.jp>
2023-05-24 01:06:01 +0900

    Chapter2-5まで。

---
+++ ch2_hashmap_match.rs
@@ -0,0 +1,15 @@
+use std::collections::HashMap;
+
+fn main() {
+    // HashMapを生成して初期化
+    let mut map = HashMap::new();
+    map.insert("A", 30);
+    map.insert("B", 50);
+
+    // キーが存在するか確認する
+    match map.get("D") {
+        None => println!("Dは存在しません"),
+        Some(v) => println!("D={}", v),
+    }
+}
+
