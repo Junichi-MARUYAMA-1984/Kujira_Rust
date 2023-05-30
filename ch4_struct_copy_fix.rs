74fecb9c37e2f190be2b74e9dbf56543bfde6e04
Junichi MARUYAMA <junichi.maruyama.rg@riken.jp>
2023-05-31 00:37:35 +0900

    Chapter 4-1まで。

---
+++ ch4_struct_copy_fix.rs
@@ -0,0 +1,24 @@
+struct Person {
+    name: String,
+    age: i32,
+}
+impl Person {
+    fn new(name: &str, age: i32) -> Self {
+        Self{ name: name.to_string(), age }
+        // Person{ name: name.to_string(), age } // こちらでも通る
+    }
+}
+
+fn main() {
+    // Taroを作成
+    let taro = Person::new("Taro", 18);
+    // JiroはTaroを複製して名前だけ変えたい
+    let jiro = Person {
+        name: String::from("Jiro"),
+        ..taro // 更新記法
+    };
+    // TaroとJiroを表示
+    println!("{}, {}", taro.name, taro.age);
+    println!("{}, {}", jiro.name, jiro.age);
+}
+
